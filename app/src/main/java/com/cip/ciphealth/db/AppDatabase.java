6b093717c1123e38953369bcfbfcbdeb2f32549d
Hesam Asnaashari <hessamasna@yahoo.com>
2023-01-17 16:37:31 +0330

    [ADD] CaloriesTracker Model & CaloriesTracker Dao

--- app/src/main/java/com/cip/ciphealth/db/AppDatabase.java
+++ app/src/main/java/com/cip/ciphealth/db/AppDatabase.java
@@ -7,14 +7,18 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.cip.ciphealth.model.CaloriesTracker;
 import com.cip.ciphealth.model.Diet;
 import com.cip.ciphealth.model.User;
+import com.cip.ciphealth.model.WeightTracker;
 
-@Database(entities = {User.class, Diet.class}, version = 1)
+@Database(entities = {User.class, Diet.class, WeightTracker.class, CaloriesTracker.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract UserDao userDao();
     public abstract DietDao dietDao();
+    public abstract WeightDao weightDao();
+    public abstract CaloriesDao caloriesDao();
 
     private static AppDatabase INSTANCE;
 
---
+++ app/src/main/java/com/cip/ciphealth/db/CaloriesDao.java
@@ -0,0 +1,26 @@
+package com.cip.ciphealth.db;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.cip.ciphealth.model.CaloriesTracker;
+
+import java.util.List;
+
+
+@Dao
+public interface CaloriesDao {
+    @Query("SELECT * FROM caloriestracker ")
+    List<CaloriesTracker> getAllCaloriesTracker();
+
+    @Insert
+    void insertCaloriesTracker(CaloriesTracker... CaloriesTrackers);
+
+    @Update
+    void update(CaloriesTracker caloriesTracker);
+
+    @Query("DELETE FROM caloriestracker")
+    void deleteAll();
+}
