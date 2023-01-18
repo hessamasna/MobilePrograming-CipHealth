17f283117676871080ac71a7c93700af58168fdb
Hesam Asnaashari <hessamasna@yahoo.com>
2023-01-18 13:25:46 +0330

    [ADD] Welcome Page

--- app/src/main/java/com/cip/ciphealth/MainActivity.java
+++ app/src/main/java/com/cip/ciphealth/MainActivity.java
@@ -1,7 +1,9 @@
 package com.cip.ciphealth;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    sleep(3*1000);
+
+                    Intent i=new Intent(getBaseContext(),Login.class);
+                    startActivity(i);
+
+                    finish();
+                } catch (Exception e) {
+                }
+            }
+        };
+        background.start();
     }
 }
