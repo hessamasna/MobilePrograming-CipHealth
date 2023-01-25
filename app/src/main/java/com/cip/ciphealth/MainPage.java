a0b9e2734413b00a6c2fea71075b9327338e2d93
Hesam Asnaashari <hessamasna@yahoo.com>
2023-01-25 20:48:07 +0330

    [ADD] Chart

--- app/src/main/java/com/cip/ciphealth/MainPage.java
+++ app/src/main/java/com/cip/ciphealth/MainPage.java
@@ -4,13 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.cip.ciphealth.adapter.HealthTipsAdapter;
 import com.cip.ciphealth.db.AppDatabase;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainPage extends AppCompatActivity {
-
+    LineChart lineChart;
+    ArrayList<Entry> list = new ArrayList<>();
+    LineDataSet lineDataSet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +37,54 @@ public class MainPage extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.health_tips_recycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
-        HealthTipsAdapter healthTipsAdapter = new HealthTipsAdapter(this, db.healthTipsDao().getAllHealthTips() ,db);
+        HealthTipsAdapter healthTipsAdapter = new HealthTipsAdapter(this, db.healthTipsDao().getAllHealthTips(), db);
         recyclerView.setAdapter(healthTipsAdapter);
+
+
+        lineChart = findViewById(R.id.chart1);
+
+
+        lineDataSet = new LineDataSet(list, "Weight Tracker");
+        lineDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        lineDataSet.setDrawValues(false);
+        LineData lineData = new LineData(lineDataSet);
+        lineChart.setData(lineData);
+        lineChart.getXAxis().setAxisMinimum(0);
+        lineChart.getXAxis().setAxisMaximum(30);
+        lineChart.getXAxis().setTextColor(Color.WHITE);
+        lineChart.getXAxis().setAxisLineColor(Color.WHITE);
+        lineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        lineChart.getAxisRight().setEnabled(false);
+        lineChart.getAxisLeft().setTextColor(Color.WHITE);
+        lineChart.getAxisLeft().setAxisMinimum(0);
+        lineChart.getAxisLeft().setAxisMaximum(210);
+        lineChart.getAxisLeft().setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getAxisLabel(float value, AxisBase axis) {
+                return value+" kg";
+            }
+        });
+
+        lineChart.animateY(5000);
+        lineChart.getDescription().setText("weight Tracker");
+        lineChart.getDescription().setTextColor(Color.WHITE);
+        lineChart.setBorderColor(Color.WHITE);
+        lineChart.getLegend().setEnabled(false);
+
+
+
+    }
+    public void RecordWeight(View v) {
+        EditText weight = (EditText) findViewById(R.id.weight_tracker_input);
+        int day = new Date().getDate();
+        list.add(new Entry((float) day, Float.parseFloat(weight.getText().toString())));
+
+        lineDataSet = new LineDataSet(list,"Weight");
+        lineDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        lineDataSet.setDrawValues(false);
+        LineData lineData = new LineData(lineDataSet);
+        lineChart.setData(lineData);
+        lineChart.notifyDataSetChanged(); // let the chart know it's data changed
+        lineChart.invalidate();
     }
 }
