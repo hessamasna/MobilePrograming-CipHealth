44250862bc5dadfa84c8987aae0edea93075402a
a.rahimzadeh <a.rahimzadeh@emofid.com>
2023-02-02 18:35:50 +0330

    Add trackers and update calories.

--- app/src/main/java/com/cip/ciphealth/MainActivity.java
+++ app/src/main/java/com/cip/ciphealth/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.cip.ciphealth.db.AppDatabase;
+import com.cip.ciphealth.model.Diet;
+import com.cip.ciphealth.model.FoodRecipe;
 import com.cip.ciphealth.model.HealthTips;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,21 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AppDatabase db = AppDatabase.getDbInstance(this.getApplicationContext());
 
-//        HealthTips healthTips = new HealthTips("test Title","Tips message","https://yahoo.com","2022");
-//        HealthTips healthTips1 = new HealthTips("test Title1","Tips messageTips messageTips messageTips message","https://yahoo.com","2022");
-//        HealthTips healthTips2 = new HealthTips("test Title2","Tips message3","https://yahoo1.com","2024");
-//        db.healthTipsDao().insertHealthTips(healthTips);
-//        db.healthTipsDao().insertHealthTips(healthTips1);
-//        db.healthTipsDao().insertHealthTips(healthTips2);
+//        addData();
 
         Thread background = new Thread() {
             public void run() {
                 try {
-                    sleep(3*1000);
+                    sleep(3 * 1000);
 
-                    Intent i=new Intent(getBaseContext(),Login.class);
+                    Intent i = new Intent(getBaseContext(), Login.class);
                     startActivity(i);
 
                     finish();
@@ -38,5 +34,32 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         background.start();
+    }
+
+    public void addData() {
+        AppDatabase db = AppDatabase.getDbInstance(this.getApplicationContext());
+
+        db.healthTipsDao().deleteAll();
+        db.dietDao().deleteAll();
+
+        HealthTips healthTips = new HealthTips("Do not skip breakfast", "Skipping breakfast will not help you lose weight. You could miss out on essential nutrients and you may end up snacking more throughout the day because you feel hungry.", "https://www.nhs.uk/better-health/lose-weight/", "2023");
+        HealthTips healthTips1 = new HealthTips("Eat plenty of fruit and veg", "Fruit and veg are low in calories and fat, and high in fibre – 3 essential ingredients for successful weight loss. They also contain plenty of vitamins and minerals.", "https://www.nhs.uk/live-well/eat-well/5-a-day/", "2023");
+        HealthTips healthTips2 = new HealthTips("Drink plenty of water", "People sometimes confuse thirst with hunger. You can end up consuming extra calories when a glass of water is really what you need.", "https://www.nhs.uk/live-well/eat-well/food-guidelines-and-food-labels/water-drinks-nutrition/", "2023");
+        HealthTips healthTips3 = new HealthTips("Read food labels", "Knowing how to read food labels can help you choose healthier options. Use the calorie information to work out how a particular food fits into your daily calorie allowance on the weight loss plan.", "https://www.nhs.uk/live-well/eat-well/food-guidelines-and-food-labels/how-to-read-food-labels/", "2023");
+        HealthTips healthTips4 = new HealthTips("Do not ban foods", "Do not ban any foods from your weight loss plan, especially the ones you like. Banning foods will only make you crave them more. There's no reason you cannot enjoy the occasional treat as long as you stay within", "https://www.nhs.uk/live-well/healthy-weight/managing-your-weight/12-tips-to-help-you-lose-weight/", "2023");
+        db.healthTipsDao().insertHealthTips(healthTips);
+        db.healthTipsDao().insertHealthTips(healthTips1);
+        db.healthTipsDao().insertHealthTips(healthTips2);
+        db.healthTipsDao().insertHealthTips(healthTips3);
+        db.healthTipsDao().insertHealthTips(healthTips4);
+
+        Diet diet = new Diet("DASH Diet", "Weight Loss Diets", "The DASH diet, which stands for dietary approaches to stop hypertension, is a flexible, balanced and heart-healthy eating plan promoted by the National Heart, Lung and Blood Institute to do exactly that: to stop (or prevent) hypertension, also referred to as high blood pressure.", "https://health.usnews.com/best-diet/dash-diet");
+        Diet diet1 = new Diet("Mediterranean Diet", "Diabetes Diets", "The Mediterranean diet is based on the traditional way of eating in the 21 countries that border the Mediterranean Sea, including Greece, Italy, Croatia, Lebanon, Turkey and Monaco. Obviously, the people who live in this diverse area eat different types of food. The bottom line is that, according to years of research and evidence, primarily eating plant-based foods, such as fruits and vegetables, while incorporating whole grains, beans, nuts, seafood, lean poultry and unsaturated fat from extra-virgin olive oil is incredibly good for overall well-being.", "https://health.usnews.com/best-diet/mediterranean-diet");
+        Diet diet2 = new Diet("3-Day Diet", "Fast Diets", "The military diet, also called the 3-day diet or the 3-day military diet, is a low-calorie plan that’s designed for quick weight loss. The military diet claims that you can lose 10 pounds in a week. The first three days of the diet follow an extremely low-calorie menu, while during the following four days, you are free to eat as you normally would.", "https://health.usnews.com/best-diet/3-day-diet");
+        Diet diet3 = new Diet("Flexitarian Diet", "Healthy Eating Diets", "Flexitarian is a marriage of two words: flexible and vegetarian. The term was coined more than a decade ago by registered dietitian Dawn Jackson Blatner in her 2009 book, \"The Flexitarian Diet: The Mostly Vegetarian Way to Lose Weight, Be Healthier, Prevent Disease and Add Years to Your Life.\"", "https://health.usnews.com/best-diet/flexitarian-diet");
+        db.dietDao().insertDiet(diet);
+        db.dietDao().insertDiet(diet1);
+        db.dietDao().insertDiet(diet2);
+        db.dietDao().insertDiet(diet3);
     }
 }
--- app/src/main/java/com/cip/ciphealth/MainPage.java
+++ app/src/main/java/com/cip/ciphealth/MainPage.java
@@ -4,15 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.cip.ciphealth.adapter.HealthTipsAdapter;
 import com.cip.ciphealth.db.AppDatabase;
+import com.cip.ciphealth.model.CaloriesTracker;
+import com.cip.ciphealth.model.Diet;
+import com.cip.ciphealth.model.FoodRecipe;
 import com.cip.ciphealth.model.LoggedInUser;
+import com.cip.ciphealth.model.User;
 import com.cip.ciphealth.model.WeightTracker;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.AxisBase;
@@ -25,11 +34,13 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class MainPage extends AppCompatActivity {
     LineChart lineChart;
     ArrayList<Entry> list = new ArrayList<>();
     LineDataSet lineDataSet;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +56,10 @@ public class MainPage extends AppCompatActivity {
 
 
         lineChart = findViewById(R.id.chart1);
+        List<WeightTracker> weightTrackers = db.weightDao().getAllWeightTrackersOfUser(LoggedInUser.getLoggedInUser().getUser().getID());
+        for (WeightTracker tracker : weightTrackers) {
+            list.add(new Entry(Float.parseFloat(tracker.getDate()), Float.parseFloat(tracker.getWeight())));
+        }
 
 
         lineDataSet = new LineDataSet(list, "Weight Tracker");
@@ -64,7 +79,7 @@ public class MainPage extends AppCompatActivity {
         lineChart.getAxisLeft().setValueFormatter(new ValueFormatter() {
             @Override
             public String getAxisLabel(float value, AxisBase axis) {
-                return value+" kg";
+                return value + " kg";
             }
         });
 
@@ -74,9 +89,9 @@ public class MainPage extends AppCompatActivity {
         lineChart.setBorderColor(Color.WHITE);
         lineChart.getLegend().setEnabled(false);
 
-
-
+        UpdateCalorie();
     }
+
     public void RecordWeight(View v) {
         AppDatabase db = AppDatabase.getDbInstance(this.getApplicationContext());
         Date date = new Date();
@@ -84,7 +99,7 @@ public class MainPage extends AppCompatActivity {
         int day = date.getDate();
         list.add(new Entry((float) day, Float.parseFloat(weight.getText().toString())));
 
-        lineDataSet = new LineDataSet(list,"Weight");
+        lineDataSet = new LineDataSet(list, "Weight");
         lineDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
         lineDataSet.setDrawValues(false);
         LineData lineData = new LineData(lineDataSet);
@@ -92,13 +107,82 @@ public class MainPage extends AppCompatActivity {
         lineChart.notifyDataSetChanged(); // let the chart know it's data changed
         lineChart.invalidate();
 
-        WeightTracker weightTracker = new WeightTracker(LoggedInUser.getLoggedInUser().getUser().getID(),weight.getText().toString(),String.valueOf(date.getDate()));
+        WeightTracker weightTracker = new WeightTracker(LoggedInUser.getLoggedInUser().getUser().getID(), weight.getText().toString(), String.valueOf(date.getDate()));
         db.weightDao().insertWeightTracker(weightTracker);
 
     }
+
+    public void UpdateCalorie() {
+        TextView calorie = (TextView) findViewById(R.id.calorie_result);
+        AppDatabase db = AppDatabase.getDbInstance(this.getApplicationContext());
+        Date date = new Date();
+
+        List<CaloriesTracker> caloriesTrackers = db.caloriesDao().getAllCaloriesTrackerByUserId(LoggedInUser.getLoggedInUser().getUser().getID(), String.valueOf(date.getDate()));
+
+        int sum = 0;
+        for (CaloriesTracker tracker : caloriesTrackers) {
+            sum += Integer.parseInt(tracker.getCalorie());
+        }
+        calorie.setText("" + sum);
+    }
+
+    public void RecordCalorie(View v) {
+        AppDatabase db = AppDatabase.getDbInstance(this.getApplicationContext());
+        EditText calorie = (EditText) findViewById(R.id.calorie_tracker_input2);
+        Date date = new Date();
+
+        CaloriesTracker caloriesTracker = new CaloriesTracker(LoggedInUser.getLoggedInUser().getUser().getID(), calorie.getText().toString(), "", String.valueOf(date.getDate()));
+        db.caloriesDao().insertCaloriesTracker(caloriesTracker);
+        UpdateCalorie();
+    }
+
     public void GotoRecipeSocial(View v) {
         Intent secondActivityIntent = new Intent(
                 getApplicationContext(), FoodRecipeSocial.class
+        );
+        startActivity(secondActivityIntent);
+    }
+
+    public void showPopupDiet(View v) {
+        //for add new recipe
+
+        AppDatabase db = AppDatabase.getDbInstance(this.getApplicationContext());
+
+        Dialog myDialog = new Dialog(this);
+        myDialog.setContentView(R.layout.diet_popup);
+        TextView diet_popup_title, diet_popup_goal, diet_popup_plan, diet_close, diet_link;
+
+        Diet diet = db.dietDao().getDietWithId(LoggedInUser.getLoggedInUser().getUser().getDietID()).get(0);
+        diet_popup_title = (TextView) myDialog.findViewById(R.id.diet_popup_title);
+        diet_popup_goal = (TextView) myDialog.findViewById(R.id.diet_popup_goal);
+        diet_popup_plan = (TextView) myDialog.findViewById(R.id.diet_popup_plan);
+        diet_link = (TextView) myDialog.findViewById(R.id.diet_link);
+        diet_close = (TextView) myDialog.findViewById(R.id.diet_close);
+
+        diet_popup_title.setText(diet.getDietName());
+        diet_popup_goal.setText(diet.getDietGoal());
+        diet_popup_plan.setText(diet.getDietText());
+
+        diet_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+        diet_link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(diet.getDietLink()));
+                MainPage.this.startActivity(browserIntent);
+            }
+        });
+
+        myDialog.show();
+    }
+
+    public void GotoProfile(View v) {
+        Intent secondActivityIntent = new Intent(
+                getApplicationContext(), Profile.class
         );
         startActivity(secondActivityIntent);
     }
